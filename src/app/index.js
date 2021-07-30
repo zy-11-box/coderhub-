3912e92ea43820ac8c179d56db1a293d48166520
zhuoya <2327524047@qq.com>
2021-07-30 19:11:58 +0800

    封装了一个路由管理函数

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,14 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
-const userRouter = require('../router/user.router')
-const authRouter = require('../router/auth.router')
+const useRoutes = require('../router')
 const errHandle = require('../app/err-handle')
+
 const app = new Koa()
+
 app.use(bodyparser())
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
+
+app.useRoutes = useRoutes
+app.useRoutes()
+
 app.on('error', errHandle)
 module.exports = app
